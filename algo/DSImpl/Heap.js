6212fe3b53df459c9940bb7cf1c20bf53eed5fca
Marco Ng <marcong95@163.com>
2019-06-26 14:33:49 +0800

    implement heapify and correct siftDown

--- algo/DSImpl/Heap.js
+++ algo/DSImpl/Heap.js
@@ -12,7 +12,15 @@ class BinaryHeap {
     heap.tree = arr
 
     if (heapify) {
-      throw new Error('heapify not implemented')
+      // heapify from the first non-leaf node
+      const firstNonLeaf = Math.floor(heap.size / 2 - 1)
+      // console.log('heapify ' + heap.toArray() + ' from #' + firstNonLeaf)
+
+      for (let i = firstNonLeaf; i >= 0; i--) {
+        // console.log(`heapify #${i}(${heap.tree[i]})`)
+        heap.siftDown(i)
+        // console.log('    got ' + heap.toArray())
+      }
     }
 
     return heap
@@ -64,14 +72,16 @@ class BinaryHeap {
       right = this._getRightChildIndex(index)
       let largest = index
 
-      if (left <= this.size && this.compare(this.tree[index], this.tree[left]) < 0) {
+      if (left <= this.size && this.compare(this.tree[largest], this.tree[left]) < 0) {
         largest = left
       }
-      if (right <= this.size && this.compare(this.tree[index], this.tree[right]) < 0) {
+      if (right <= this.size && this.compare(this.tree[largest], this.tree[right]) < 0) {
         largest = right
       }
 
       if (largest !== index) {
+        // console.log(`swapping #${index}(${this.tree[index]}) &` +
+        //   ` #${largest}(${this.tree[largest]})`)
         this._swap(index, largest)
       } else {
         return
@@ -110,9 +120,14 @@ module.exports = {
 }
 
 if (require && require.main === module) {
-  const heap = BinaryHeap.fromArray([9, 5, 8, 4], MAX_HEAP, false)
-  heap.push(10)
-  console.log(heap.toArray())
-  heap.pop()
-  console.log(heap.toArray())
+  // test for push or pop
+  const heap1 = BinaryHeap.fromArray([9, 5, 8, 4], MAX_HEAP, false)
+  heap1.push(10)
+  console.log(heap1.toArray())
+  heap1.pop()
+  console.log(heap1.toArray())
+
+  // test for heapify
+  const heap2 = BinaryHeap.fromArray([7, 5, 19, 8, 4, 1, 20, 13, 16])
+  console.log(heap2.toArray())
 }
