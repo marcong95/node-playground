b8bec9f24be06b1c9c7915a59e01a4ed2fd82ca0
Marco Ng <marcong95@163.com>
2020-01-14 16:58:45 +0800

    add eink demo; ignore dbdef; modify duplicateRows

--- database/duplicateRows.js
+++ database/duplicateRows.js
@@ -1,28 +1,31 @@
 const uuid = require('uuid')
-// const { Op } = require('sequelize')
-const { sequelize, Report } = require('./dbdef-iot')
+const { sequelize, Patient } = require('./dbdef-iot')
 
 const whereToDuplicate = {
   mrNumber: 'BILI2333'
 }
-const duplicateAmount = 100
+const duplicateAmount = 50
 
-// Report.destroy({
+// const { Op } = require('sequelize')
+// Patient.destroy({
 //   where: {
-//     guid: { [Op.like]: '2333abcd%' }
+//     mrNumber: { [Op.like]: 'BILI2333_%' }
 //   }
 // })
 // return /* eslint-disable no-unreachable, no-useless-return */
-Report.findAll({
+
+Patient.findAll({
   where: whereToDuplicate
 }).then(data => {
   if (data.length <= 0) {
     console.error('no record found with specified condition')
   }
-  return Report.bulkCreate(new Array(duplicateAmount)
+  return Patient.bulkCreate(new Array(duplicateAmount)
     .fill(null)
-    .map((_v, idx) => Object.assign({}, data[idx % data.length].get(), {
-      guid: uuid().replace(/\w{8}/, '2333abcd')
+    .map((_null, idx) => Object.assign({}, data[idx % data.length].get(), {
+      guid: uuid().replace(/\w{8}/, '2333abcd'),
+      mrNumber: data[idx % data.length].get('mrNumber') + `_${idx}`,
+      createdAt: new Date(2019, 6, 4, 23, idx + Math.floor(idx / 60), idx % 60)
     })))
 }).catch(err => {
   console.error(err.message)
